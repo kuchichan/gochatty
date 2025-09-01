e018d0a0dfe2bb40d533436e4308b18daae2dd66
kuchi <pawel.kucharski@hotmail.com>
2025-09-01 22:52:08 +0200

    feat: ready to broadcast messages

--- main.go
+++ main.go
@@ -1,25 +1,115 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"html/template"
 	"log"
 	"log/slog"
 	"net/http"
 	"os"
-	"time"
 
 	"github.com/gorilla/websocket"
 )
 
+type WsClient struct {
+	conn   *websocket.Conn
+	toSend chan []byte
+	logger *slog.Logger
+	room   *Room
+}
+
+type Room struct {
+	broadcast chan PayloadDecoder
+	logger    *slog.Logger
+}
+
+func NewRoom(logger *slog.Logger) *Room {
+	return &Room{
+		broadcast: make(chan PayloadDecoder),
+		logger:    logger,
+	}
+}
+
+func (r *Room) run() {
+	for {
+		select {
+		case msg := <-r.broadcast:
+			r.logger.Info("room", "broadcast", msg)
+		}
+	}
+
+}
+
+type Message struct {
+	MsgType string          `json:"type"`
+	Payload json.RawMessage `json:"payload"`
+}
+
+type PayloadDecoder interface {
+	DecodeMsg()
+}
+
+type JoinMsg struct {
+	Name string `json:"userName"`
+}
+
+func (m *Message) DecodeMessage(data []byte) (PayloadDecoder, error) {
+	err := json.Unmarshal(data, &m)
+	if err != nil {
+		return nil, err
+	}
+	var payload PayloadDecoder
+
+	switch m.MsgType {
+	case "join":
+		payload = new(JoinMsg)
+		json.Unmarshal(m.Payload, payload)
+	}
+
+	return payload, nil
+}
+
+func (jm *JoinMsg) DecodeMsg() {
+}
+
+func (c *WsClient) readLoop() {
+	for {
+		_, message, err := c.conn.ReadMessage()
+		message = bytes.TrimSpace(message)
+		msg := new(Message)
+
+		c.logger.Info("ws", "received", string(message))
+		payload, err := msg.DecodeMessage(message)
+		if err != nil {
+			c.logger.Error("decode", "decoder-error", err.Error())
+		}
+		c.logger.Info("ws", "received-payload", payload)
+
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				c.logger.Error("ws-error", "read-message", err)
+			}
+			break
+		}
+
+		c.room.broadcast <- payload
+	}
+}
+
 func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
 	t := template.New("home")
 	template, err := t.ParseFiles("./home.tmpl.html")
-	upgrader := websocket.Upgrader{}
-
 	if err != nil {
 		log.Fatalf("template: %s", err.Error())
 	}
+
+	upgrader := websocket.Upgrader{}
+
+	room := NewRoom(logger)
+	go room.run()
 
 	fileServer := http.FileServer(http.Dir("./static"))
 
@@ -37,21 +127,12 @@ func main() {
 		conn, err := upgrader.Upgrade(w, r, nil)
 
 		if err != nil {
-			log.Println(err)
+			logger.Error("ws-error", "connection-error", err)
 			return
 		}
+		client := WsClient{conn: conn, logger: logger, toSend: make(chan []byte, 256), room: room}
 
-		go func(c *websocket.Conn) {
-			for {
-				err = c.WriteMessage(websocket.TextMessage, []byte("Hello!"))
-				if err != nil {
-					logger.Error("ws-error", "write-message", err.Error())
-				}
-				time.Sleep(20 * time.Second)
-				logger.Info("Write hello...")
-			}
-		}(conn)
-		time.Sleep(2500 * time.Millisecond)
+		go client.readLoop()
 
 	})
 
--- static/js/main.js
+++ static/js/main.js
@@ -2,28 +2,56 @@ console.log("Hello!");
 
 const socket = new WebSocket("ws://localhost:4000/ws");
 document.addEventListener('DOMContentLoaded', () => {
-  let ws = null
-  const form = document.getElementById("formJoin");
-  const userInput = document.getElementById("userInput");
-  const status = document.getElementById("status");
+    let ws = null
+    const form = document.getElementById("formJoin");
+    const userInput = document.getElementById("userInput");
+    const status = document.getElementById("status");
+    const submitBtn = document.getElementById("submitBtn");
 
-  form.addEventListener("submit", function(ev) {
-    ev.preventDefault();
-    const userName = userInput.value.trim();
-    if (!userName) {
-      alert("please enter your name!");
-      return;
-
+    const updateStatus = (message, type) => {
+        status.textContent = message;
+        status.classList = `status ${type}`
+        status.style.display = "block";
     }
-    console.log(`Name: ${userName}`);
 
-  })
+    const initializeWebsocket = (userName) => {
+        updateStatus('Connecting websocket...', 'connecting');
+        submitBtn.disabled = true;
 
-  window.addEventListener('beforeunload', () => {
-    if (ws) {
-      ws.close();
+        ws = new WebSocket("http://localhost:4000/ws")
+
+        ws.onopen = (event) => {
+            console.log("Websocket connected!");
+            updateStatus('websocket connected!', 'connected')
+
+            const joinMsg = {
+                type: "join",
+                payload: {
+                    userName: userName
+                }
+            }
+            ws.send(JSON.stringify(joinMsg));
+        }
     }
-  })
+
+    form.addEventListener("submit", function(ev) {
+        ev.preventDefault();
+        const userName = userInput.value.trim();
+        if (!userName) {
+            alert("please enter your name!");
+            return;
+
+        }
+        console.log(`Name: ${userName}`);
+        initializeWebsocket(userName);
+
+    })
+
+    window.addEventListener('beforeunload', () => {
+        if (ws) {
+            ws.close();
+        }
+    })
 })
 
 
